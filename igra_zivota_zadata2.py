3ba52314602030a2def0b43af14bf91a93f15df3
kalugege <kalugege@gmail.com>
2020-10-27 16:47:17 +0100

    igra_zivota_zadatak2

--- igra_zivota_zadata2.py
+++ igra_zivota_zadata2.py
@@ -5,38 +5,30 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
 import sys
+import queue
 N = 4
 matrica=np.random.randint(2,size=(N,N))
-#matrica =   [[0,0,0,0],
-           # [0,1,0,0],
-          #  [0,0,1,1],
-          #  [1,0,0,1]]
 listaMatrica = []
 brojacCelija = 0
 
 print(matrica) 
 
 brojacCelijaLock = threading.Lock()
-procitanOdLock = threading.Lock()
 sledecaIteracija=threading.Condition()
 
 class Celija(threading.Thread):
     def __init__(self,stanje,i,j):
         super().__init__()
         self.iteracija=0
-        self.procitanOd = [0,0,0,0,0,0,0,0]
+        
         self.stanje=stanje
         self.i=i
         self.j=j
-        self.semaforCelije = threading.Semaphore(0)    
+        self.stanjeKomsija = queue.Queue(maxsize=8)    
 
     
     
     def proveraSuseda(self):
-        #lista = []
-        
-        ziveKomsije=0
-        pozicija=0
         for x in range(self.i - 1, self.i + 2):
             for y in range(self.j - 1, self.j + 2):
                 if x == self.i and y == self.j:
@@ -46,28 +38,19 @@ class Celija(threading.Thread):
                     x=x % N
                 if y < 0 or y>=N:
                     y = y % N
-                self.azurirajListu(x,y,pozicija)
-                pozicija+=1
-                ziveKomsije+=matrica[x,y]    
-               #lista.append((x,y))
-                
-        return ziveKomsije
-
-   
-    def azurirajListu(self,x,y,pozicija):
-        procitanOdLock.acquire()
+                self.azurirajQueue(x,y)      
+                   
+    
+    def azurirajQueue(self,x,y):
         for celija in listaCelija:
             if celija.i == x and celija.j == y:
-                celija.procitanOd[pozicija]+=1
-                if all(celija.procitanOd):
-                    celija.semaforCelije.release()
-        procitanOdLock.release()         
-
-        
-
-
-            
+                celija.stanjeKomsija.put(self.stanje)
+                 
+    
+ 
                 
+           
+          
 
 
     def promenaStanja(self,ziveKomsije):
@@ -91,23 +74,23 @@ class Celija(threading.Thread):
         brojacCelijaLock.release()
 
         self.iteracija+=1
-
+        
     def run(self):
         global brojacCelija
-        
+       
         for _ in range(0,3):
-         
-            ziveKomsije = self.proveraSuseda()
-            self.semaforCelije.acquire()
-            procitanOdLock.acquire()
-            for i in range(0,8):
-                self.procitanOd[i]=0
-            procitanOdLock.release()
-            time.sleep(1)
-            #print(self.procitanOd)
+            self.proveraSuseda()
+            ziveKomsije=0
+            for _ in range(0,8):
+                
+                ziveKomsije+=self.stanjeKomsija.get()
+                
+            
+            time.sleep(0.1)
+            
            
             self.promenaStanja(ziveKomsije)
-            #sys.stdout.write(f'{self.stanje}')
+            
             brojacCelijaLock.acquire()
             if brojacCelija == N * N:
                 brojacCelija=0
@@ -140,12 +123,7 @@ for matrica in listaMatrica:
    print(matrica)
 
 
-#for celija in listaCelija:
-  #  celija.proveraSuseda()
 
-#for celija in listaCelija:
- #   print(celija.procitanOd)
- #   print(celija.semaforCelije._value) 
 
    
 
